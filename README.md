60d05ac281fa774bf214cca0db09b95946af8610
Sam Stonehouse <sgstonehouse@googlemail.com>
2021-09-16 23:13:14 +0100

    Fixed colour picking bug, added hover effect to results

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 A small app which shows the process of polygon decomposition, turning a concave polycon into a collection of convex polygons.
 
 The poly-decomp library is a TypeScript port of https://github.com/schteppe/poly-decomp.js
+
+## Running
+
+`yarn run dev`
+
+## First Time Setup
+
+`yarn install`
--- source/poly-shape.tsx
+++ source/poly-shape.tsx
@@ -1,9 +1,36 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Vector3, BufferGeometry, Shape, Vector2 } from 'three';
 
+/**
+ * https://stackoverflow.com/a/7638362/873401
+ */
 const randomColor = () => {
-  // eslint-disable-next-line no-bitwise
-  return `#${((1 << 24) * Math.random() | 0).toString(16)}`;
+  let c = '';
+  while (c.length < 6) {
+    c += (Math.random()).toString(16).substr(-6).substr(-1);
+  }
+  return `#${c}`;
+};
+
+/**
+ * https://stackoverflow.com/a/62640342
+ */
+const colorShade = (_col, amt) => {
+  let col = _col.replace(/^#/, '');
+  if (col.length === 3) col = col[0] + col[0] + col[1] + col[1] + col[2] + col[2];
+
+  let [r, g, b] = col.match(/.{2}/g);
+  ([r, g, b] = [parseInt(r, 16) + amt, parseInt(g, 16) + amt, parseInt(b, 16) + amt]);
+
+  r = Math.max(Math.min(255, r), 0).toString(16);
+  g = Math.max(Math.min(255, g), 0).toString(16);
+  b = Math.max(Math.min(255, b), 0).toString(16);
+
+  const rr = (r.length < 2 ? '0' : '') + r;
+  const gg = (g.length < 2 ? '0' : '') + g;
+  const bb = (b.length < 2 ? '0' : '') + b;
+
+  return `#${rr}${gg}${bb}`;
 };
 
 interface IPolyShapeProps {
@@ -12,8 +39,14 @@ interface IPolyShapeProps {
   line?: boolean,
 }
 
+
+
 const PolyShape = ({ points, color = '#09060c', line = false }: IPolyShapeProps): React.ReactElement | null => {
   const polyRef = useRef<BufferGeometry>(null!);
+
+  const [createdColor] = useState(randomColor());
+  const [shadedColor] = useState(colorShade(createdColor, -40));
+  const [mouseOver, setMouseOver] = useState(false);
 
   if (line) {
     useEffect(() => {
@@ -36,8 +69,11 @@ const PolyShape = ({ points, color = '#09060c', line = false }: IPolyShapeProps)
   const shape = new Shape(points.map(([x, y]) => new Vector2(x, y)));
 
   return (
-    <mesh>
-      <meshBasicMaterial color={randomColor()} attach='material' />
+    <mesh
+      onPointerEnter={() => { setMouseOver(true); }}
+      onPointerLeave={() => { setMouseOver(false); }}
+    >
+      <meshBasicMaterial color={mouseOver ? shadedColor : createdColor} attach='material' />
       <shapeGeometry attach='geometry' args={[shape]} />
     </mesh>
   );
